2dad90463bc94b51fb4371604f16c8d40622ef3c
Naveen Belkale <bnaveenbhat@gmail.com>
2023-09-20 12:25:41 +0530

    Initial commit getting Scala 3 to work with dependencies

--- 01_scala_tutorial/src/main/scala/cmd/Runner.scala
+++ 01_scala_tutorial/src/main/scala/cmd/Runner.scala
@@ -1,7 +1,21 @@
 package cmd
 
+import java.io.{File, IOException}
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.text.{PDFTextStripper, TextPosition}
 import lib.Greeting
 
+object PDFExtractor {
+  def extractTextFromPDF(input: Array[Byte]): String = {
+    val document = Loader.loadPDF(input)
+    val textStripper = new PDFTextStripper()
+    textStripper.setSortByPosition(true)
+    textStripper.setAddMoreFormatting(true)
+    val text = textStripper.getText(document)
+    document.close()
+    text
+  }
+}
 object Runner {
   def main(args: Array[String]) = {
     Greeting.sayHi
